9b1242765d396647ba4b9ec4aed7e8de426b29b6
hamatatsu <tatsuyahamada.g@gmail.com>
2021-07-12 21:02:10 +0900

    Add sleep 1sec

--- client/__main__.py
+++ client/__main__.py
@@ -1,11 +1,13 @@
 from .arduino import Arduino
 from logging.config import fileConfig
 import datetime
+import time
 
 date = datetime.datetime.now().strftime("%y%m%d%H%M%S")
 fileConfig("client/logging.conf", defaults={"date":date}, disable_existing_loggers=False)
 
 with Arduino() as arduino:
+  time.sleep(1)
   arduino.start()
   try:
     while True:
