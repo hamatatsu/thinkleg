2978bc44acdc447ad87f9dd27db1948c7492d1f2
hamatatsu <6399551+hamatatsu@users.noreply.github.com>
2021-10-20 13:53:15 +0000

    Add mqtt submit

--- client/__main__.py
+++ client/__main__.py
@@ -1,17 +1,27 @@
-from .arduino import Arduino
-from logging.config import fileConfig
 import datetime
+import sys
 import time
+from logging.config import fileConfig
+import paho.mqtt.client as mqtt
+
+from .arduino import Arduino
 
 date = datetime.datetime.now().strftime("%y%m%d%H%M%S")
 fileConfig("client/logging.conf", defaults={"date":date}, disable_existing_loggers=False)
+
+client = mqtt.Client()
+client.username_pw_set("ham", "thinkleg")
+client.connect(sys.argv[1], 1883, 60)
+client.loop_start()
 
 with Arduino() as arduino:
   time.sleep(1)
   arduino.start()
   try:
     while True:
-      arduino.read()
+      data = arduino.read()
       arduino.save_csv(f"log/{date}.csv")
+      if data:
+        client.publish("test", data[0].decode().split(',')[1].strip())
   except KeyboardInterrupt:
     exit()
